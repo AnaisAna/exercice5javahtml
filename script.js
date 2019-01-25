29af1487842633db922d0381a9569e79f71f10c1
AnaisAna <anais.fradin@laposte.net>
2019-01-25 10:41:58 +0100

    exercice6.html

---
+++ script.js
@@ -0,0 +1,6 @@
+function firstImage(img){
+document.getElementById(img).src='images/' + img + '_2.jpg' ;
+}
+function secondImage(img){
+document.getElementById(img).src='images/' + img + '.jpg';
+}
